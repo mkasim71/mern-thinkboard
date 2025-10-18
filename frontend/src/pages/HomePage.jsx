7808513a14dc188ee91e144401ce2bd4c10e25ab
Muhammad Kasim <mkasim77@gmail.com>
2025-10-18 12:06:02 +0700

    First Commit - thinkboard

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import Navbar from "../components/Navbar";
+import RateLimitedUI from "../components/RateLimitedUI";
+
+const HomePage = () => {
+  const [isRateLimited, setIsRateLimited] = useState(true);
+
+  return (
+    <div className="min-h-screen">
+      <Navbar />
+
+      {isRateLimited && <RateLimitedUI />}
+    </div>
+  );
+};
+
+export default HomePage;
